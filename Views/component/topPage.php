9dd9495ae1efe5bd6ae62a518f709ed237b7368c
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-04-22 12:05:25 +0900

    editorを動的に言語を変更できるように修正

--- Views/component/topPage.php
+++ Views/component/topPage.php
@@ -8,8 +8,8 @@
     <div class="d-flex flex-column align-items-center py-4 card col-12 col-sm-8 my-4 mx-auto col-md-6 ">
         <h3>Options</h3>
         <div class="col-12 p-2">
-            <label for="validTime" class="pb-1">Syntax HighLight: </label>
-            <select id="validTime" name="validTime" class="form-select ">
+            <label for="syntax_highlight" class="pb-1">Syntax HighLight: </label>
+            <select id="syntax_highlight" name="syntax_highlight" class="form-select">
                 <?php foreach ($syntaxHighLight as $key => $value) {
                 ?>
                     <option value=<?php echo $key; ?> class=""><?php echo $key; ?></option>;
@@ -18,7 +18,7 @@
         </div>
         <div class="col-12 p-2">
             <label for="validTime" class="pb-1">Expireatoin Time: </label>
-            <select id="validTime" name="validTime" class="form-select ">
+            <select id="validTime" name="validTime" class="form-select">
                 <?php foreach ($validTime as $time) {
                 ?>
                     <option value=<?php echo $time; ?> class=""><?php echo $time; ?></option>;
@@ -39,8 +39,6 @@
 <!-- Monaco Editor Scripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/loader.min.js"></script>
 <script>
-    const input_ele = document.getElementById("input-ele");
-
     require.config({
         paths: {
             'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs'
@@ -49,22 +47,60 @@
 
     require(['vs/editor/editor.main'], function() {
         let editor = monaco.editor.create(document.getElementById('editor-container'), {
-            value: "",
+            value: "ここにテキストを入力してください\n",
             language: 'plaintext'
         });
 
-        editor.onDidChangeModelContent(() => {
+        let create_snippet = document.getElementById("create_snippet");
+        create_snippet.addEventListener("click", () => {
+            let syntax_highlight = document.getElementById("syntax_highlight");
+            let validTime = document.getElementById("validTime");
+            let title = document.getElementById("title");
+            let editorEle = document.getElementById("editor-container");
+            title = !title.value ? "undefined" : title.value;
             let value = editor.getValue();
 
-            if (png.value == "on") {
-                updateRender(value, "png");
-            } else if (svg.value == "on") {
-                updateRender(value, "svg");
-            } else if (text.value == "on") {
-                updateRender(value, "txt");
-            } else {
-                updateRender(value, "uml");
-            }
+            const jsonBody = JSON.stringify({
+                syntax_highlight: syntax_highlight.value,
+                title: title,
+                validTime: validTime.value,
+                data: value
+            })
+
+            const requet = new Request("../../Helpers/createURL.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    syntax_highlight: syntax_highlight.value,
+                    title: title,
+                    validTime: validTime.value,
+                    data: value
+                })
+            })
+
+            fetch(requet)
+                .then(response => response.json())
+                .then(data => {
+                    console.log("DDD", data);
+                })
+        })
+
+
+        let syntax_highlight = document.getElementById("syntax_highlight");
+        console.log(syntax_highlight);
+        syntax_highlight.addEventListener("change", () => {
+            let value = syntax_highlight.value;
+            let model = editor.getModel();
+
+            monaco.editor.setModelLanguage(model, value);   
+            console.log(value);
+        })
+
+        editor.onDidChangeModelContent(() => {
+            // let value = editor.getValue();
+            // console.log(value);
         });
-    });
+    })
 </script>
